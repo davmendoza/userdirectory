7f9721267f56e3506f59c55494cd3f1d2ed7126e
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-11-16 20:59:54 -0800

    updated employeetable

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -6,12 +6,13 @@ import NameFilterInput from "./NameFilterInput";
 import compareNames from "../utils/compareNames";
 
 function EmployeeTable(props) {
+    const [filteredEmployees, setFilteredEmployees] = useState(props.employees);
     const [filterState, setFilterState] = useState("");
     const [sortDirection, setSortDirection] = useState(1);
 
-    let employeeRecords = props.employees.slice(0);
+    //let employeeRecords = props.employees.slice(0);
 
-    employeeRecords.sort(compareNames(sortDirection));
+
 
     function toggleSortDirection(event) {
         event.preventDefault();
@@ -19,27 +20,46 @@ function EmployeeTable(props) {
         setSortDirection(-1 * sortDirection);
     }
 
+    useEffect(function () {
+        let filteredRecords = props.employees;
+
+        if (filterState !== '') {
+             filteredRecords = props.employees
+            .filter(employeeRecord => {
+                return employeeRecord.name.first.startsWith(filterState)
+                    || employeeRecord.name.last.startsWith(filterState);
+
+            })
+            
+        }
+        filteredRecords
+                .sort(compareNames(sortDirection));
+
+            setFilteredEmployees(filteredRecords);
+    }, [props.employees, sortDirection, filterState]);
+
     function onFilterChange(event) {
-        const {value} = event.target;
+        const { value } = event.target;
 
         setFilterState(value);
     }
 
     return (
         <div>
+            <br />
             <NameFilterInput filterState={filterState} onFilterChange={onFilterChange} ></NameFilterInput>
-            <br></br>
+            <br />
             <table>
                 <thead>
                     <tr>
                         <th><NameSortLink toggleSortDirection={toggleSortDirection} /> </th>
                     </tr>
                 </thead>
-                <br></br>
+
                 <tbody>
                     {
 
-                        employeeRecords.map(employeeRecord =>
+                        filteredEmployees.map(employeeRecord =>
                             (<EmployeeRow employeeRecord={employeeRecord} key={employeeRecord.id.value} />))
                     }
                 </tbody>
